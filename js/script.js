95e439c02ceb4b5bb48566e6fff3d859c97146aa
Shubh Mangukiya <shubh.mangukiya01@gmail.com>
2024-06-01 18:44:12 +0530

    cng

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ function saveEmailAsJson(event) {
 }
 
 function fetchEmails() {
-    fetch('/emails')
+    fetch('placeaa.com/emails.json')
         .then(response => response.json())
         .then(data => {
             const emailList = document.getElementById('emailList');
